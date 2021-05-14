c77041f653bbfd491cc8358363a5f2015fc6a06d
Roli <rroycehg@gmail.com>
2021-05-13 20:37:58 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+
+const main = document.querySelector('main');
+
+main.remove();
+
+
+
+const newHeader = document.createElement('h1');
+const currentDiv = document.getElementById("div1");
+
+newHeader.id = 'victory';
+
+const champ = document.createTextNode("Roli is the champion");
+newHeader.appendChild(champ);
+
